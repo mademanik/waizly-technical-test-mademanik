1d10b938a1da6b7d4b5a535a34cceaf960821d7d
grasshoppermademanik <11584657+mademanik@users.noreply.github.com>
2024-03-23 15:52:27 +0700

    feat: add backend sql query test answer

---
+++ mysql-docker/sql_export/employees_202403231320.sql
@@ -0,0 +1,7 @@
+INSERT INTO waizly_db.employees (name,job_title,salary,department,joined_date) VALUES
+	 ('John Smith','Manager',60000.0,'Sales','2022-01-15'),
+	 ('Jane Doe','Analyst',45000.0,'Marketing','2022-02-01'),
+	 ('Mike Brown','Developer',55000.0,'IT','2022-03-10'),
+	 ('Anna Lee','Manager',65000.0,'Sales','2021-12-05'),
+	 ('Mark Wong','Developer',50000.0,'IT','2023-05-20'),
+	 ('Emily Chen','Analyst',48000.0,'Marketing','2023-06-02');
